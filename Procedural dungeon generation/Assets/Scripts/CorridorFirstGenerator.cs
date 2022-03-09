3dc744bc9500b54a389416b8adf9d130f4d9f7ec
Sem Clous <sem.clous@gmail.com>
2022-03-09 15:22:31 +0100

    BSP, key and knife

--- Procedural dungeon generation/Assets/Scripts/CorridorFirstGenerator.cs
+++ Procedural dungeon generation/Assets/Scripts/CorridorFirstGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static PCGAlgorithms;
 
 public class CorridorFirstGenerator : SimpleWalkGenerator
 {
@@ -25,9 +26,44 @@ public class CorridorFirstGenerator : SimpleWalkGenerator
 
         HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPositions);
 
+        List<Vector2Int> deadEnds = FindAllDeadEnds(floorPositions);
+
+        CreateRoomsAtDeadEnds(deadEnds, roomPositions);
+
         floorPositions.UnionWith(roomPositions);
 
         tilemapVisualizer.PaintFloorTiles(floorPositions);
+    }
+
+    private void CreateRoomsAtDeadEnds(List<Vector2Int> deadEnds, HashSet<Vector2Int> roomFloors)
+    {
+        foreach (var position in deadEnds)
+        {
+            if (roomFloors.Contains(position) == false)//if no room is attached to this position the position presents an actual dead end.
+            {
+                var room = RandomWalk(randomWalkParameters, position);//create a room at actual dead ends
+                roomFloors.UnionWith(room);
+            }
+        }
+    }
+
+    private List<Vector2Int> FindAllDeadEnds(HashSet<Vector2Int> floorPositions)
+    {
+        List<Vector2Int> deadEnds = new List<Vector2Int>();
+        foreach (var position in floorPositions)
+        {
+            int neighboursCount = 0;
+            foreach (var direction in Direction2D.cardinalDirectionList)
+            {
+                if (floorPositions.Contains(position + direction))//check for neighbouring tiles and add them to the neighbour count for each position in floorpositions
+                    neighboursCount++;
+            }
+
+            if (neighboursCount == 1)//if a tile has only a single neighbour, that means it is a dead end so we add it to our deadends list
+                deadEnds.Add(position);
+        }
+
+        return deadEnds;
     }
 
     private HashSet<Vector2Int> CreateRooms(HashSet<Vector2Int> potentialRoomPositions)
--- Procedural dungeon generation/Assets/Scripts/PCGAlgorithms.cs
+++ Procedural dungeon generation/Assets/Scripts/PCGAlgorithms.cs
@@ -35,11 +35,78 @@ public class PCGAlgorithms : MonoBehaviour
 
         return corridor;
     }
-}
 
-public static class Direction2D
-{
-    public static List<Vector2Int> cardinalDirectionList = new List<Vector2Int>
+    public static List<BoundsInt> BinarySpacePartitioning(BoundsInt spaceToSplit, int minWidth, int minHeight)//boundsInt represents a bounding box
+    {
+        Queue<BoundsInt> roomsQueue = new Queue<BoundsInt>();
+        List<BoundsInt> roomsList = new List<BoundsInt>();
+
+        roomsQueue.Enqueue(spaceToSplit);
+        while (roomsQueue.Count > 0)
+        {
+            var room = roomsQueue.Dequeue();
+            if (room.size.y >= minHeight && room.size.x >= minWidth)//room.size represents the size of our bounding box which we check for our minimum sizes, and create rooms only if they are greater than said size
+            {
+                if (Random.value < 0.5f)//split horizontally first unless impossible then check for vertical, random.value ensures a more random variation of horizontally and vertically split rooms
+                {
+                    if (room.size.y >= minHeight * 2)//if it's large enough to fit another 2 rooms and have space left then split accordingly
+                    {
+                        SplitHorizontally(minHeight, roomsQueue, room);
+                    }
+                    else if (room.size.x >= minWidth * 2)
+                    {
+                        SplitVertically(minWidth, roomsQueue, room);
+                    }
+                    else
+                    {
+                        roomsList.Add(room);
+                    }
+                }
+                else//split vertically
+                {
+
+                    if (room.size.x >= minWidth * 2)
+                    {
+                        SplitVertically(minWidth, roomsQueue, room);
+                    }
+                    else if (room.size.y >= minHeight * 2)
+                    {
+                        SplitHorizontally(minHeight, roomsQueue, room);
+                    }
+                    else
+                    {
+                        roomsList.Add(room);
+                    }
+                }
+            }
+        }
+        return roomsList;
+    }
+
+
+    private static void SplitVertically(int minWidth, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    {
+        var xSplit = Random.Range(1, room.size.x);//select random point in our box to perform a split
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.size.y, room.size.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x + xSplit, room.min.y, room.min.z),//room 2's x value starting point is equal to our minimum x + the location where we perform our split, so xSplit, y and z value do not change when splitting vertically
+            new Vector3Int(room.size.x - xSplit, room.size.y, room.min.z));//The size is equal to the total room size - the split location where y and z remain the same again
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
+    }
+
+    private static void SplitHorizontally(int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    {
+        var ySplit = Random.Range(1, room.size.y);//(minHeight, room.size.y - minHeight) would ensure that we would always create a split that would ensure we can fit two rooms in one but this would create more of a grid structure
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(room.size.x, ySplit, room.size.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x, room.min.y + ySplit, room.min.z),//See SplitVertically for room2 explaination except replace xsplit with ysplit etc.
+            new Vector3Int(room.size.x, room.size.y - ySplit, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
+    }
+
+    public static class Direction2D
+    {
+        public static List<Vector2Int> cardinalDirectionList = new List<Vector2Int>
     {
         new Vector2Int(0, 1), //UP
         new Vector2Int(1, 0), //RIGHT
@@ -47,8 +114,10 @@ public static class Direction2D
         new Vector2Int(-1, 0), //LEFT
     };
 
-    public static Vector2Int GetRandomDirection()
-    {
-        return cardinalDirectionList[Random.Range(0, cardinalDirectionList.Count)];
+        public static Vector2Int GetRandomDirection()
+        {
+            return cardinalDirectionList[Random.Range(0, cardinalDirectionList.Count)];
+        }
     }
+
 }
